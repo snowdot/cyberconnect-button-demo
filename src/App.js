6b0a995181e4ae556df0475d537deed0cb509e44
snowdot <co.pascu@gmail.com>
2022-03-14 17:54:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import {
+  FollowButton,
+  Env,
+  Blockchain,
+} from "@cyberconnect/react-follow-button";
 
 function App() {
+  const [isBtnDisabled, setIsBtnDisabled] = useState(false);
+  const [address, setAddress] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [toAddress, setToAddress] = useState("");
+
+  const isMetaMaskInstalled = () => {
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  }
+
+  const handleOnClickConnect = async () => {
+    if(isMetaMaskInstalled()) {
+      setIsBtnDisabled(true);
+      try {
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+        setAddress(accounts[0]);
+      } catch (error) {
+        alert("Something went wrong.");
+      }
+      setIsBtnDisabled(false);
+    } else {
+      alert("Please install MetaMask.");
+    }
+  }
+
+  const handleOnChangeSearch = e => {
+    setSearchValue(e.target.value);
+  }
+
+  const handleOnKeyDownSearch = e => {
+    if(e.key === "Enter") {
+      setToAddress(e.target.value);
+    } else {
+      setToAddress("");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="navbar">
+        <button
+          className="connect-btn"
+          onClick={handleOnClickConnect}
+          disabled={isBtnDisabled}
         >
-          Learn React
-        </a>
-      </header>
+          {
+            address
+            ? `${address.slice(0, 4)}...${address.slice(-4)}`
+            : "Connect"
+          }
+        </button>
+      </div>
+      {
+        address &&
+        <div className="wrapper">
+          <input
+            className="search-bar"
+            value={searchValue}
+            onChange={handleOnChangeSearch}
+            onKeyDown={handleOnKeyDownSearch}
+          >
+          </input>
+          {
+            toAddress &&
+            <FollowButton
+              provider={window.ethereum}
+              namespace="CyberConnect"
+              toAddr={toAddress}
+              env={Env.STAGING}
+              chain={Blockchain.ETH}
+              onSuccess={(e) => {
+                console.log(e);
+              }}
+              onFailure={(e) => {
+                console.log(e);
+              }}
+            />
+          }
+        </div>
+      }
     </div>
   );
 }
